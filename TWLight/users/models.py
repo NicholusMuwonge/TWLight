67cf4eb51102230cd6c247532be35245a4b3783e
uyscuti-wiki <uyscuti.wiki@gmail.com>
2019-10-02 21:28:28 +0530

    return access if users don't want it anymore

--- TWLight/users/models.py
+++ TWLight/users/models.py
@@ -486,3 +486,9 @@ class Authorization(models.Model):
             return True
         else:
             return False
+
+    @property
+    def user(self):
+        # Needed by SelfOnly mixin, e.g. on the authorization return
+        # view.
+        return self.authorized_user.editor
